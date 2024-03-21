c564ebcb518d18b2204dbcc4cd2d5d863d138ba6
Kosiv92 <kosiv92@gmail.com>
2024-03-21 23:48:36 +0800

    Add API documentation fro CRUD operations in ExerciseTypeController

--- src/IG_Train.Web/Controllers/ExerciseTypeController.cs
+++ src/IG_Train.Web/Controllers/ExerciseTypeController.cs
@@ -27,6 +27,8 @@ namespace IG_Train.Web.Controllers
         /// <returns>Collection of exercise types</returns>
         [SwaggerResponse(200, "Collection of exercise types", typeof(GetExerciseTypesResponse))]
         [SwaggerResponse(400, "The exercise type data is invalid")]
+        [Produces("application/json")]
+        [Consumes("application/json")]
         [HttpGet]
         public Task<GetExerciseTypesResponse> GetExerciseTypes(CancellationToken cancellationToken) =>
             _mediator.Send(new GetExerciseTypesRequest(), cancellationToken);
@@ -46,28 +48,55 @@ namespace IG_Train.Web.Controllers
         ///        "description": "The push-up is a push or press movement that works all the pressing muscles in the upper body"
         ///     }
         ///
-        /// </remarks>
-        [HttpPost]
+        /// </remarks>        
         [SwaggerResponse(201, "The exercise type was created", typeof(CreateExerciseTypeResponse))]
         [SwaggerResponse(400, "The exercise type data is invalid")]
-        public async Task<ActionResult<CreateExerciseTypeResponse>> CreateExerciseTypeAsync(
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        [HttpPost]
+        public ActionResult<Task<CreateExerciseTypeResponse>> CreateExerciseTypeAsync(
             [FromBody] CreateExerciseTypeRequest request,
             CancellationToken cancellationToken)
             //=> await _mediator.Send(request, cancellationToken);
-        => CreatedAtAction(nameof(CreateExerciseTypeAsync), request, await _mediator.Send(request, cancellationToken));
+        => CreatedAtAction(nameof(CreateExerciseTypeAsync), request, _mediator.Send(request, cancellationToken));
 
         /// <summary>
         /// Update exist exercise type
         /// </summary>
         /// <param name="request">Data for update exist exercise type</param>
         /// <param name="cancellationToken"></param>
-        /// <returns></returns>
+        /// <returns>ID of the created exercise type</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /CreateExerciseType
+        ///     {                
+        ///        "id": "1"
+        ///        "name": "Dip and flip",
+        ///        "description": "The dip and flip is a fun move that works your triceps and biceps"
+        ///     }
+        ///
+        /// </remarks>       
+        [SwaggerResponse(200, "The exercise type was updated succefully", typeof(UpdateExerciseTypeResponse))]
+        [SwaggerResponse(400, "The exercise type update is failed")]
+        [Produces("application/json")]
+        [Consumes("application/json")]
         [HttpPut]
-        public async Task<ActionResult<UpdateExerciseTypeResponse>> UpdateExerciseType(
+        public Task<UpdateExerciseTypeResponse> UpdateExerciseType(
             [FromBody] UpdateExerciseTypeRequest request,
             CancellationToken cancellationToken)
-            => await _mediator.Send(request, cancellationToken);
-        
+            =>  _mediator.Send(request, cancellationToken);
+
+        /// <summary>
+        /// Delete exist exercise type
+        /// </summary>
+        /// <param name="request">Id of the object to be deleted</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [SwaggerResponse(200, "The exercise type was delete succefully", typeof(DeleteExerciseTypeResponse))]
+        [SwaggerResponse(400, "The exercise type delete is failed")]
+        [Produces("application/json")]
+        [Consumes("application/json")]
         [HttpDelete]
         public Task<DeleteExerciseTypeResponse> DeleteExerciseType(
             [FromBody] DeleteExerciseTypeRequest request,
